8bc22708015e9af948616f928a23753fde7bbdde
marc <marc.hervas10@gmail.com>
2019-10-03 15:01:46 +0200

    cart2

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,16 +2,19 @@ export const products = [
     {
       name: 'Green tea',
       price: 3.11,
-      id: 'GR1'
+      id: 'GR1',
+      counter: 0
     },
     {
       name: 'Strawberries',
       price: 5,
-      id: 'SR1'
+      id: 'SR1',
+      counter: 0
     },
     {
       name: 'Coffee',
       price: 11.23,
-      id: 'CF1'
+      id: 'CF1',
+      counter: 0
     }
   ];
